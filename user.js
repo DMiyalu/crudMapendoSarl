2c7740a4f76254db34971649f03a25d5dd123f34
Dido Miyalu <dieudonne.miyalu@academy.kinshasadigital.com>
2020-04-24 19:57:38 +0200

    first commit

---
+++ user.js
@@ -0,0 +1,4 @@
+let users = [
+    { nom: "Miyalu", prenom: "Dieudonné", email: "dieudonne@gmail.com", age: "23", poste: "CEO", telephone: "+243 825 937 168", statutMarital: "Celibataire", paysOrigine: "Congolais", modifier: "modifier", supprimer: "delete" },
+    { nom: "Makengo", prenom: "Stanislas", email: "stanislas@gmail.com", age: "22", poste: "CEO", telephone: "+243818832947", statutMarital: "Celibataire", paysOrigine: "Congolais", modifier: "modifier", supprimer: "delete" }
+];
